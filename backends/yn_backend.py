19a1d6860aa89140e39338166da3def9d3ef0413
Justas Brazauskas <jb2328@cam.ac.uk>
2023-05-18 16:28:41 +0100

    fixed yn_backend.py edge case

--- backends/yn_backend.py
+++ backends/yn_backend.py
@@ -10,6 +10,10 @@ class YNBackend:
     self.detector.setInputSize((w,h))
     _, faces = self.detector.detect(img)
     out = []
+
+    if faces is None or len(faces) == 0:
+        return None  # Return an empty list if no faces are detected
+
     for f in faces:
       out.append({'confidence': float(f[-1]),
                   'x': int(f[0]), 'y': int(f[1]), 'w': int(f[2]), 'h': int(f[3])})
