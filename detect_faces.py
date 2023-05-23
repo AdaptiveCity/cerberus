071a1d73daa33fe2b8d1add46d314f31b33cb76b
jb2328 <jb2328@cam.ac.uk>
2023-05-23 17:14:44 +0100

    locally loaded images

--- detect_faces.py
+++ detect_faces.py
@@ -9,21 +9,29 @@ import json
 from picamera2 import Picamera2
 from classes.jb2328_yunet import Yunet
 from classes.jb2328_haar import HaarCascade
-# from classes.face_detector import FaceDetector
+from settings import settings
+
 
 def capture(img,res):
-   
-    timestamp = time.time()
-    formatted_time = time.strftime("%Y-%m-%d_%H-%M-%S", time.localtime(timestamp))
+    #check that we're not duplicating a passed image
+    #if it's not a string then a photo, save with custom name
+    if not type(img) is str:        
+        timestamp = time.time()
+        formatted_time = time.strftime("%Y-%m-%d_%H-%M-%S", time.localtime(timestamp))
 
-    filename="cerberus_alpha_"+formatted_time+"_"+str(int(timestamp))
-    # Save the current frame as an image
-    image_name = "./img/"+filename+".jpg"
-                # Using cv2.imwrite() method
-                # Saving the image
-    cv2.imwrite(image_name,cv2.cvtColor(img, cv2.COLOR_RGB2BGR) )
+        filename="cerberus_alpha_"+formatted_time+"_"+str(int(timestamp))
+        # Save the current frame as an image
+        image_name = "./img/"+filename+".jpg"
+        # Using cv2.imwrite() method
+        # Saving the image
+        cv2.imwrite(image_name,cv2.cvtColor(img, cv2.COLOR_RGB2BGR) )
+    #else it's probably not a photo but a string name of an existing photo
+    else:
+        #save the json file with with an existing filename but different extension
+        image_name = img
+        
+    #save fd results
     outfile = image_name[:-4]+".json"
-    print(res)
     with open(outfile, 'w') as f:
         json.dump(res, f)
         print(f'Wrote JSON to {outfile}')
@@ -31,8 +39,35 @@ def capture(img,res):
             # cur.save(image_name)
     print("img captured")
 
-        
+def load_locally(model_name, image_path):
+      # Instantiate face detection object here
+    score_threshold = None  # Set the threshold if needed
 
+    #temporary if/else for debug purposes:
+    if model_name == "Yunet":
+        fd_model = Yunet(score_threshold)
+    elif model_name == "haarcascade":
+        fd_model = HaarCascade()
+    else:
+        raise ValueError("Invalid model name")
+   
+    #load the image
+    im = cv2.imread(image_path)
+
+    # Perform face detection here
+    results = fd_model.run(im)
+    
+    faces=results["faces"]
+    
+    print(results["metadata"]["model"]+" detected faces:", len(faces), 
+            " Inference duration:", results["metadata"]["inference_time"], " seconds")
+    
+    if(len(faces)>0):
+        print("RESULTS",results)
+        capture(image_path, results)
+                
+    return
+            
 def main(model_name, resolution):
     last_save = 0
     save_interval=60 #time in seconds
@@ -60,12 +95,11 @@ def main(model_name, resolution):
         raise ValueError("Invalid model name")
         
     while True:
+       
         im = picam2.capture_array()
+
         # Perform face detection here
-        start = time.time()
         results = fd_model.run(im)
-        end = time.time()
-        duration = round(end - start,2)
         
         faces=results["faces"]
         
@@ -80,17 +114,20 @@ def main(model_name, resolution):
                 capture(im, results)
                 last_save = current_time
                 
-        results=None
-        im=None
-           
 
-            
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Face detection script using Picamera2.")
     parser.add_argument("-m", "--model", type=str, default="haarcascade",
                         help="Select the face detection model. Default is 'haarcascade'.")
     parser.add_argument("-r", "--resolution", nargs=2, type=int, default=None,
                         help="Set the camera resolution. Pass width and height as two integers, e.g., '-r 1280 720'. Default is config_HQ.")
-
+    parser.add_argument("-i","--image", type=str, default=None,
+                        help="Path to the image to be processed. If this argument is provided, the script will process the provided image instead of capturing a new frame.")
+   
     args = parser.parse_args()
-    main(args.model, args.resolution)
+    print("settings:",settings)
+    if(args.image is None):
+        main(args.model, args.resolution)
+    else:
+        print("loading locally")
+        load_locally(args.model, args.image)
---
+++ settings.py
@@ -0,0 +1,2 @@
+#modify based on your settings
+settings = {"acp_id":"cerberus-undefined-lt1", "seat_capacity":100}
