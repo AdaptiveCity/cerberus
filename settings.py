a6f1b6ece71dc7437f7cc031572c296c6b484441
Justas Brazauskas <jb2328@cam.ac.uk>
2023-05-30 13:41:16 +0100

    timestamps are consistent with filename and acp_ts

--- settings.py
+++
@@ -1,7 +0,0 @@
-#modify based on your settings
-settings = {"acp_id":"cerberus-mac-lt1", 
-            "seat_capacity":100, 
-            "position":"R",
-            "model":"haarcascade", 
-            "threshold": 0.7,
-            "frequency":60}
